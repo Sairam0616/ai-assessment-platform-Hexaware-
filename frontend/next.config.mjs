14ecbbb4aa14eb3fc7951c60fd1eba43101a5e74
Sai Ram <126678956+Sairam0616@users.noreply.github.com>
2024-11-04 11:37:13 +0530

    Update next.config.mjs

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -3,8 +3,8 @@ const isProd = process.env.NODE_ENV === 'production';
 
 const nextConfig = {
   output: 'export',
-  assetPrefix: isProd ? '/<your-repo-name>/' : '',
-  basePath: isProd ? '/<your-repo-name>' : '',
+  assetPrefix: isProd ? '/<https://github.com/Sairam0616/ai-assessment-platform-Hexaware-/frontend/app/home/page.html>/' : '',
+  basePath: isProd ? '/<https://github.com/Sairam0616/ai-assessment-platform-Hexaware-/frontend/app/home/page.html>' : '',
   images: {
     unoptimized: true,
   },
